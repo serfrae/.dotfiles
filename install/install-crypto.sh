e7b2d29083630180918a8ead50d247fbe190e773
a7rs <a7raser@gmail.com>
2021-09-02 22:01:37 +0100

    some, not a lot

--- install/install-crypto.sh
+++ install/install-crypto.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-git clone "https://github.com/a7raser/polybar-crypto-rs.git" "$DOTFILES/polybar/polybar-crypto-rs"
+git clone "https://github.com/a7rs/polybar-crypto-rs.git" "$DOTFILES/polybar/polybar-crypto-rs"
 cargo build --manifest-path "$DOTFILES/polybar/polybar-crypto-rs/Cargo.toml" --release
