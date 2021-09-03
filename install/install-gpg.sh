d00c8433da34082178840e81c7e8c99ac89cefdf
a7rs <a7raser@gmail.com>
2021-09-03 09:15:12 +0100

    some, not a lot

--- install/install-gpg.sh
+++ install/install-gpg.sh
@@ -2,8 +2,7 @@
 
 mkdir -p $XDG_DATA_HOME/gnupg
 
-gpg --import $HOME/misc/.keys/pgp-public-keys.asc
-gpg --import $HOME/misc/.keys/pgp-private-keys.asc
-
 chmod 600 $XDG_DATA_HOME/gnupg/*
 chmod 700 $XDG_DATA_HOME/gnupg
+
+gpg --import $HOME/misc/.keys/pgp-private-keys.asc
