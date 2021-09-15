ee183de9ae98f61ee69f514bb5cdcb31f98c2a4d
a7rs <a7raser@gmail.com>
2021-09-15 10:31:10 +0100

    some, not a lot

--- install-way.sh
+++ install-way.sh
@@ -7,6 +7,11 @@ function dot_install() {
   $DOTFILES/install/install-${1}.sh
 }
 
+function way_install() {
+  echo -e "Installing ${1} config"
+  $DOTFILES/install/way/install-${1}.sh
+}
+
 dot_install dirs
 dot_install zsh
 rm -rf $HOME/*bash*
@@ -16,16 +21,16 @@ dot_install alacritty
 dot_install ctags
 dot_install dfk
 dot_install git
-dot_install mako
+way_install mako
 dot_install mutt
 dot_install npm
 dot_install nvim
 dot_install scripts
-dot_install sway
+way_install sway
 dot_install tmux
 dot_install urlview
 dot_install yarn
-dot_install waybar
+way_install waybar
 dot_install zathura
 
 dot_install apps
--- install-x.sh
+++ install-x.sh
@@ -7,26 +7,31 @@ function dot_install() {
   $DOTFILES/install/install-${1}.sh
 }
 
+function x_install() {
+  echo -e  "Installing ${1} config"
+  $DOTFILES/install/x/install-${1}.sh
+}
+
 dot_install dirs
 dot_install zsh
 chmod -w $HOME
 
 dot_install alacritty
-dot_install bspwm
+x_install bspwm
 dot_install ctags
-dot_install dunst
+x_install dunst
 dot_install git
 dot_install mutt
 dot_install npm
 dot_install nvim
-dot_install picom
-dot_install polybar
-dot_install rofi
+x_install picom
+x_install polybar
+x_install rofi
 dot_install scripts
 dot_install sxhkd
 dot_install tmux
 dot_install urlview
-dot_install X11
+x_install X11
 dot_install yarn
 dot_install zathura
 
