435067ae4b84f7a50fe4a3c469b7ad24da47327b
a7rs <a@serfrae.com>
2022-01-11 18:16:18 +0800

    lol

--- install/install-dfk.sh
+++ install/install-dfk.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo mkdir /etc/interception
-sudo ln -sf $DOTFILES/dual-function-keys/udevmon.yaml /etc/interception/udevmon.yaml
+sudo ln -sf $DOTFILES/dfk/udevmon.yaml /etc/interception/udevmon.yaml
 sudo systemctl enable udevmon
 sudo systemctl start udevmon
