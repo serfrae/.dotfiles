622bf2358c77907d8fb352d714a0f644b6ef8c6e
a7rs <a@serfrae.com>
2024-02-08 16:49:51 +0000

    fix ctags

--- install/install-ctags.sh
+++ install/install-ctags.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/universal-ctags/ctags.git $XDG_DATA_HOME/ctags
 cd "$XDG_DATA_HOME/ctags"
 $XDG_DATA_HOME/ctags/autogen.sh \\
-$XDG_DATA_HOME/configure --prefix=$HOME \\
+$XDG_DATA_HOME/ctags/configure --prefix=$HOME \\
 make && make install \\
 mkdir -p "$NOTES_DIR/.ctags.d"
 ln -sf "$DOTFILES/ctags/md.ctags" "$NOTES_DIR/.ctags.d/md.ctags"
