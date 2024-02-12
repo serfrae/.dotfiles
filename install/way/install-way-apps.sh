c383879b4ab707035257bbf6faafc600c36519a7
serfrae <a@serfrae.com>
2024-02-12 18:58:35 +0000

    some, not a lot

--- install/way/install-way-apps.sh
+++ install/way/install-way-apps.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo pacman -S grim mako pipewire slurp sway swaybg swayidle swaylock waybar wf-recorder wget wl-clipboard xdg-desktop-portal-wlr
+sudo pacman -S grim mako pipewire pipewire-pulse slurp sway swaybg swayidle swaylock waybar wf-recorder wget wl-clipboard xdg-desktop-portal-wlr
