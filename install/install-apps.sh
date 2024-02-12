7aab22b4bbbadb8bf49cdd0ec528e27809403bfa
serfrae <a@serfrae.com>
2024-02-12 19:01:11 +0000

    some, not a lot

--- install/install-apps.sh
+++ install/install-apps.sh
@@ -4,7 +4,7 @@
 sudo pacman -S archlinux-keyring
 
 # Install packages
-sudo pacman -S alacritty alsa-utils bluez bluez-utils cargo clang cmake docker eza fd fzf github-cli htop interception-tools interception-dual-function-keys jq libnotify npm openssh python python-pip ripgrep rustup sd texlive-core tmux ttf-font-awesome ttf-jetbrains-mono ttf-nerd-fonts-symbols wget xdg-user-dirs zsh zsh-syntax-highlighting zathura zathura-pdf-mupdf zsh-autosuggestions
+sudo pacman -S alacritty alsa-utils bluez bluez-utils cargo clang cmake docker eza fd fzf github-cli htop interception-tools interception-dual-function-keys jq libnotify npm nnn openssh python python-pip ripgrep rustup sd texlive-core tmux ttf-font-awesome ttf-jetbrains-mono ttf-nerd-fonts-symbols wget xdg-user-dirs zsh zsh-syntax-highlighting zathura zathura-pdf-mupdf zsh-autosuggestions
 
 # Install Rust
 rustup install nightly
