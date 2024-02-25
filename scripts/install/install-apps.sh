95c591aa2c059d761aa18b9ca590d422974c8c6e
serfrae <a@serfrae.com>
2024-02-25 09:13:34 +0000

    some, not a lot

--- scripts/install/install-apps.sh
+++ scripts/install/install-apps.sh
@@ -4,7 +4,7 @@
 sudo pacman -S archlinux-keyring
 
 # Install packages
-sudo pacman -S alacritty alsa-utils bluez bluez-utils brightnessctl cargo clang cmake docker eza fd fzf github-cli htop interception-tools interception-dual-function-keys jq libnotify npm nnn openssh python python-pip ripgrep rustup sd texlive-core tmux ttf-font-awesome ttf-jetbrains-mono ttf-nerd-fonts-symbols wget
+sudo pacman -S alacritty alsa-utils bluez bluez-utils brightnessctl cargo clang cmake docker eza fd fish fzf github-cli htop interception-tools interception-dual-function-keys jq libnotify npm nnn openssh python python-pip ripgrep rustup sd texlive-core tmux ttf-font-awesome ttf-jetbrains-mono ttf-nerd-fonts-symbols wget
 
 # Install Rust
 rustup install nightly
@@ -13,12 +13,11 @@ rustup target add wasm32-unknown-unknown --toolchain nightly
 cargo install wasm-pack
 cargo install cargo-outdated
 
-# Install yay
-git clone "https://aur.archlinux.org/yay.git" "~/.local/share/yay"
-cd "~/.local/share/yay"
-makepkg -si
+# Install fisher
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher install 
 
-yay -S autojump-rs
+paru -S autojump-rs
 
 # Install PIP packages
 pip install --user --upgrade pynvim
