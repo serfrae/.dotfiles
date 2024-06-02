b982a204c3505ab1aaedfceac1f5c77047033f76
serfrae <a@serfrae.com>
2024-06-02 17:20:04 +0100

    remove venv-selector from init.lua as scripts/autocmds/alias do the job fine for now

--- nvim/init.lua
+++ nvim/init.lua
@@ -726,16 +726,4 @@ require("lazy").setup({
   {
     "sheerun/vim-polyglot"
   },
-  {
-    'linux-cultist/venv-selector.nvim',
-    dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
-    lazy = false,
-    opts = {
-      auto_refresh = true,
-      dap_enabled = true,
-    },
-    keys = {
-      { '<leader>v', '<cmd>:VenvSelect<cr>' },
-    },
-  }
 })
