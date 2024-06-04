9c71ae8532eac1cb0b8537f50aa15667fee2a7ff
serfrae <a@serfrae.com>
2024-06-04 23:31:52 +0100

    some, not a lot

--- fish/config.fish
+++ fish/config.fish
@@ -44,6 +44,8 @@ function fish_prompt
         echo -n (basename $PWD)
     end
     set -g __fish_git_prompt_show_informative_status 1
+    set -g __fish_git_prompt_showupstream 'informative'
+    set -g __fish_git_prompt_showstashstate 1
     set -g __fish_git_prompt_showcolorhints 1
     set_color green
     printf '%s ' (__fish_git_prompt)
