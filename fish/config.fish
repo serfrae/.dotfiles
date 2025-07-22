24e1c901e2ee472089f4c65d35ab6bcb8a88b8b7
Andrew Fraser <serfrae@pm.me>
2025-07-22 13:56:28 -0400

    some, not a lot

--- fish/config.fish
+++ fish/config.fish
@@ -111,7 +111,7 @@ end
 
 function fish_user_key_bindings
     fish_default_key_bindings -M insert
-    #fish_vi_key_bindings --no-erase insert
+    fish_vi_key_bindings --no-erase insert
 end
 
 pixi completion --shell fish | source
