3520f0147abad83451a055b19de2803e851af6fb
Andrew Fraser <a@serfrae.com>
2026-01-08 11:30:34 -0500

    some, not a lot

--- nvim/lua/plugins/ui/catppuccin.lua
+++ nvim/lua/plugins/ui/catppuccin.lua
@@ -5,8 +5,7 @@ return {
         priority = 1000,
         config = function()
             vim.cmd([[colorscheme catppuccin]])
-            local bools = vim.api.nvim_get_hl(0, { name = 'Boolean' })
-            vim.api.nvim_set_hl(0, 'Comment', bools)
+            vim.api.nvim_set_hl(0, 'Comment', { fg = '#89dceb', italic = true })
         end
     }
 }
